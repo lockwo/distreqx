8d99e48276fb756a742ac480749e516401f52c6a
Mayalen Etcheverry <etcheverry.mayalen@gmail.com>
2024-06-10 17:57:55 +0000

    categorical distribution

--- distreqx/distributions/__init__.py
+++ distreqx/distributions/__init__.py
@@ -2,6 +2,7 @@ from ._distribution import (
     AbstractDistribution as AbstractDistribution,
 )
 from .bernoulli import Bernoulli as Bernoulli
+from .categorical import Categorical as Categorical
 from .independent import Independent as Independent
 from .mvn_diag import MultivariateNormalDiag as MultivariateNormalDiag
 from .mvn_from_bijector import (
