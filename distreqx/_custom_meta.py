c12db4d9e6c3a6bfbdfb18eb556febd113fc9c69
Owen Lockwood <42878312+lockwo@users.noreply.github.com>
2025-07-09 18:35:32 -0700

    ihoop

---
+++ distreqx/_custom_meta.py
@@ -0,0 +1,17 @@
+import equinox as eqx
+import ihoop
+
+
+try:
+
+    class _StrictEqxMeta(ihoop.strict._StrictMeta, eqx._module._module._ModuleMeta):
+        def __new__(mcs, name, bases, namespace, **kwargs):
+            return super().__new__(mcs, name, bases, namespace, **kwargs)
+
+    class StrictModule(eqx.Module, ihoop.Strict, metaclass=_StrictEqxMeta):  # type: ignore[reportRedeclaration]
+        def __init_subclass__(cls, *, strict: bool | None = None, **kwargs):
+            super().__init_subclass__(**kwargs)
+except Exception:
+
+    class StrictModule(eqx.Module):
+        pass
