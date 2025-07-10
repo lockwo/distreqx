fefc9c6d61d9adfe19c747d4d087d4f8e242436c
Owen Lockwood <42878312+lockwo@users.noreply.github.com>
2025-07-09 18:42:03 -0700

    format

--- distreqx/_custom_meta.py
+++ distreqx/_custom_meta.py
@@ -1,3 +1,5 @@
+from dataclasses import dataclass
+
 import equinox as eqx
 import ihoop
 
@@ -8,6 +10,7 @@ try:
         def __new__(mcs, name, bases, namespace, **kwargs):
             return super().__new__(mcs, name, bases, namespace, **kwargs)
 
+    @dataclass(frozen=True)
     class StrictModule(eqx.Module, ihoop.Strict, metaclass=_StrictEqxMeta):  # type: ignore[reportRedeclaration]
         def __init_subclass__(cls, *, strict: bool | None = None, **kwargs):
             super().__init_subclass__(**kwargs)
