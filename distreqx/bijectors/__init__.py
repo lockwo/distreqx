98b5be051f126c8e9d3e1949bf273d7b6fe026b8
Gary Allen <gvcallen@gmail.com>
2026-07-01 22:57:11 +0200

    Add Exp and Softplus bijectors
    Exponential and softplus bijectors modeled after TensorFlow
    Probability's implementations, mapping the real line to the positive
    domain.

--- distreqx/bijectors/__init__.py
+++ distreqx/bijectors/__init__.py
@@ -7,10 +7,12 @@ from ._bijector import (
 from ._block import Block as Block
 from ._chain import Chain as Chain
 from ._diag_linear import DiagLinear as DiagLinear
+from ._exp import Exp as Exp
 from ._linear import AbstractLinearBijector as AbstractLinearBijector
 from ._scalar_affine import ScalarAffine as ScalarAffine
 from ._shift import Shift as Shift
 from ._sigmoid import Sigmoid as Sigmoid
+from ._softplus import Softplus as Softplus
 from ._tanh import Tanh as Tanh
 from ._triangular_linear import TriangularLinear as TriangularLinear
 from ._unconstrained_affine import UnconstrainedAffine as UnconstrainedAffine
